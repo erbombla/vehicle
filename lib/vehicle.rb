57f434f54aa886ed118dacc3d7e22f652a6317df
Eric <ebomblatus@gmail.com>
2016-11-30 10:24:22 -0800

    add methods for specs

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -1,6 +1,34 @@
 class Vehicle
+  @@vehicles = []
+
   def initialize (make, model, year)
+    @make = make
+    @model = model
     @year = year
+  end
+
+  def make
+    @make
+  end
+
+  def model
+    @model
+  end
+
+  def year
+    @year
+  end
+
+  define_singleton_method(:all) do
+    @@vehicles
+  end
+
+  def save
+    @@vehciles.push(self)
+  end
+
+  define_singleton_method(:clear) do 
+    @@vehciles = []
   end
 
   def age
@@ -14,5 +42,4 @@ class Vehicle
     new_enough = age().<=(50)
     japanese and new_enough
   end
-
 end
