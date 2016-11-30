b7d6384a91f6dae3bbcd02463e04e8aad5ce78a5
Eric <ebomblatus@gmail.com>
2016-11-30 09:20:04 -0800

    pass first spec

--- spec/vehicle_spec.rb
+++ spec/vehicle_spec.rb
@@ -1,8 +1,11 @@
-require 'rspec'
-require 'vehicle'
+require ('rspec')
+require ('vehicle')
 
-describe(Vehicle) do
+describe('Vehicle') do
   describe('#age') do
     it('returns age of vehicle') do
-      test_vehicle = Vehicle.new('Ford')
+      test_vehicle = Vehicle.new('Datsun', '240z', 1972)
+      expect(test_vehicle.age()).to(eq(44))
+    end
   end
+end
